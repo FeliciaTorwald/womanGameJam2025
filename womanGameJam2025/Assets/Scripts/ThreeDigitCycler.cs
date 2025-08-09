719d0e769a684c8cde7392f929828d4ae9246998
FeliciaTorwald <113012104+FeliciaTorwald@users.noreply.github.com>
2025-08-09 12:27:22 +0200

    code lock

---
+++ womanGameJam2025/Assets/Scripts/ThreeDigitCycler.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ThreeDigitCycler : MonoBehaviour
+{
+    public Button[] digitButtons;       
+    public TextMeshProUGUI[] digitTexts; 
+    public string correctCode = "735";
+
+    void Start()
+    {
+        for (int i = 0; i < digitButtons.Length; i++)
+        {
+            int index = i;  
+            digitButtons[i].onClick.AddListener(() => CycleDigit(index));
+            digitTexts[i].text = "0";  
+        }
+    }
+
+    void CycleDigit(int buttonIndex)
+    {
+        int currentDigit = int.Parse(digitTexts[buttonIndex].text);
+        currentDigit = (currentDigit + 1) % 10;
+        digitTexts[buttonIndex].text = currentDigit.ToString();
+
+        CheckCode();
+    }
+
+    void CheckCode()
+    {
+        string enteredCode = "";
+        foreach (var txt in digitTexts)
+        {
+            enteredCode += txt.text;
+        }
+
+        if (enteredCode == correctCode)
+        {
+            Debug.Log("Code correct! Puzzle solved.");
+            
+        }
+    }
+}
