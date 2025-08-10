fac2a21367984c43995f02a4260fe21668a235e1
FeliciaTorwald <113012104+FeliciaTorwald@users.noreply.github.com>
2025-08-10 15:43:23 +0200

    Update LightSwitch.cs

--- womanGameJam2025/Assets/Scripts/LightSwitch.cs
+++ womanGameJam2025/Assets/Scripts/LightSwitch.cs
@@ -2,9 +2,22 @@ using UnityEngine;
 
 public class LightSwitch : MonoBehaviour
 {
-    public Light[] lightsToToggle;  
+    public Light[] lightsToToggle;
+
+    [Header("Sounds")]
+    public AudioClip turnOnSound;
+    public AudioClip turnOffSound;
 
     private bool lightsOn = true;
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        // Ensure we have an AudioSource to play the sounds
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+            audioSource = gameObject.AddComponent<AudioSource>();
+    }
 
     void OnMouseDown()
     {
@@ -13,6 +26,16 @@ public class LightSwitch : MonoBehaviour
         foreach (Light light in lightsToToggle)
         {
             light.enabled = lightsOn;
+        }
+
+        // Play the correct sound
+        if (lightsOn && turnOnSound != null)
+        {
+            audioSource.PlayOneShot(turnOnSound);
+        }
+        else if (!lightsOn && turnOffSound != null)
+        {
+            audioSource.PlayOneShot(turnOffSound);
         }
     }
 }
